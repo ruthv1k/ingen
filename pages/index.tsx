f2086155d76859ad6532b443489a5ed2baf19311
ruthvik <ruthvik.web@gmail.com>
2022-03-09 18:39:18 +0530

    I-0: Create basic next project with required config
    created a next project using create-next-app
    extended tailwind colors to add custom colors to the project
    added paths in tsconfig for path mapping
    added Inter as default font for all the text
    changed background color to light-background

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Simple Task Planner</title>
+      </Head>
+      <div>
+        <h1>Simple Task Planner</h1>
+      </div>
+    </>
+  )
+}
+
+export default Home
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'light-background': '#f0f3ff',
+        'light-theme-primary': '#3F3CBB',
+      },
+    },
+  },
+  plugins: [],
+}
