3ee87a88d1327cce181eb0d00222e80cee9538a9
ruthvik <ruthvik.local>
2022-09-08 09:05:38 +0530

    Add button, icon components
    - `buttons` contains ToggleThemeButton, and other buttons that are reused
    - `icons` contains all the SVGs used in the project

---
+++ components/buttons/buttons.tsx
@@ -0,0 +1,21 @@
+import { MoonIcon, SunIcon } from '@/components/icons'
+import { Theme } from 'types'
+
+export const ToggleThemeButton = ({
+  themeContext,
+}: {
+  themeContext: Theme | null
+}) => {
+  return (
+    <button
+      className="mx-4 dark:text-white"
+      onClick={() =>
+        themeContext?.setCurrentTheme(
+          themeContext?.currentTheme === 'dark' ? 'light' : 'dark'
+        )
+      }
+    >
+      {themeContext?.currentTheme === 'dark' ? <SunIcon /> : <MoonIcon />}
+    </button>
+  )
+}
