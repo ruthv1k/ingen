af7542dd00f24b27394606e69477f5ea6093783a
ruthvik <ruthvik.local>
2022-09-29 19:01:55 +0530

    Fix today's cell styling issue
    - isToday, isPast returns true for today's date, today's cell styles are overridden due to this.
    - isToday check is now done ahead of isPast to fix the issue

--- src/components/calendar/calendar.cell.tsx
+++ src/components/calendar/calendar.cell.tsx
@@ -3,11 +3,11 @@ import { format, isPast, isToday } from 'date-fns'
 const styles = {
   calendarButton: {
     pastDays:
-      'absolute top-0 left-0 z-0 flex h-full w-full items-center justify-center transition-all duration-150 ease-linear border-light-border/30 border border-light-border-pale font-normal text-light-text-pale dark:border-dark-theme-primary/20 dark:bg-dark-theme-primary/20 dark:text-white/25',
-    currentDay:
-      'absolute top-0 left-0 z-10 flex h-full w-full items-center justify-center transition-all duration-150 ease-linear border-2 border-light-theme-primary bg-white/30 hover:bg-light-theme-primary/25  hover:font-semibold hover:text-white dark:border-dark-theme-primary dark:bg-dark-theme-primary/5 dark:text-white dark:hover:bg-dark-theme-primary/50',
+      'absolute top-0 left-0 z-0 flex h-full w-full items-center justify-center transition-all duration-150 ease-linear border-light-border/30 border border-light-border-pale font-normal text-light-text-pale dark:border-dark-theme-primary/20 dark:bg-dark-theme-primary/20 dark:text-white/25 outline-none',
+    today:
+      'absolute top-0 left-0 z-10 flex h-full w-full items-center justify-center transition-all duration-150 ease-linear border-2 border-light-theme-primary bg-white/30 hover:bg-light-theme-primary/25  hover:font-semibold hover:text-white dark:border-dark-theme-primary dark:bg-dark-theme-primary/5 dark:text-white dark:hover:bg-dark-theme-primary/50 outline-none',
     upcomingDays:
-      'absolute top-0 left-0 z-0 flex h-full w-full items-center justify-center transition-all duration-150 ease-linear border border-light-theme-primary/25 bg-white/30 hover:border-light-theme-primary/25 hover:bg-light-theme-primary/10 hover:font-semibold hover:text-light-theme-primary dark:border-dark-theme-primary/25 dark:bg-dark-theme-primary/5 dark:text-white dark:hover:border-dark-theme-primary  dark:hover:bg-dark-theme-primary/50',
+      'absolute top-0 left-0 z-0 flex h-full w-full items-center justify-center transition-all duration-150 ease-linear border border-light-theme-primary/25 bg-white/30 hover:border-light-theme-primary/25 hover:bg-light-theme-primary/10 hover:font-semibold hover:text-light-theme-primary dark:border-dark-theme-primary/25 dark:bg-dark-theme-primary/5 dark:text-white dark:hover:border-dark-theme-primary  dark:hover:bg-dark-theme-primary/50 outline-none',
   },
 }
 
@@ -24,10 +24,10 @@ const CalendarCell = ({
         value={format(day, 'PP')}
         onClick={handlePopup}
         className={
-          isPast(day)
+          isToday(day)
+            ? styles.calendarButton.today
+            : isPast(day)
             ? styles.calendarButton.pastDays
-            : isToday(day)
-            ? styles.calendarButton.currentDay
             : styles.calendarButton.upcomingDays
         }
       >
